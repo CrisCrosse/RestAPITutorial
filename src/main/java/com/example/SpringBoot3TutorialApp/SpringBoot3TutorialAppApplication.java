44cdc0e2abed5811f653bc93a4d026342a4e55cb
Chris Rossell <chris.rossell@autotrader.co.uk>
2024-11-13 17:04:35 +0000

    initial commit

---
+++ src/main/java/com/example/SpringBoot3TutorialApp/SpringBoot3TutorialAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot3TutorialApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3TutorialAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3TutorialAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBoot3TutorialApp/SpringBoot3TutorialAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot3TutorialApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3TutorialAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
